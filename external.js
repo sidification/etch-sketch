c01b1fd5fe5a719c2cb9e962fda91104db3cc646
Sid Ahuja <ahujass@c001808661.mw.na.cat.com>
2023-06-06 10:34:11 -0500

    add black hover

--- external.js
+++ external.js
@@ -1,12 +1,20 @@
 const container = document.querySelector(".container")
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
   const sqRow = document.createElement("div")
   sqRow.setAttribute("class", "square-row")
-  for (let j = 0; j < 4; j++) {
+  for (let j = 0; j < 16; j++) {
     const square = document.createElement("div")
     square.setAttribute("class", "square")
     sqRow.appendChild(square)
   }
   container?.appendChild(sqRow)
 }
+
+const squares = document.querySelectorAll('.square')
+// console.log(square.length)
+squares.forEach((square) => {
+  square.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = 'black';
+  })
+});
